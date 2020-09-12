0d1f33a4eac9ac462882c999006ee9cc1d3fa76b
Wilson Styres <wstyres@gmail.com>
2020-09-11 23:14:52 -0400

    [Home] Download and cache featured packages

--- Zebra/Tabs/Home/ZBHomeViewController.m
+++ Zebra/Tabs/Home/ZBHomeViewController.m
@@ -10,7 +10,15 @@
 
 #import "Settings/ZBMainSettingsTableViewController.h"
 
-@interface ZBHomeViewController ()
+#import <ZBAppDelegate.h>
+#import <ZBLog.h>
+#import <Tabs/Sources/Helpers/ZBSource.h>
+#import <Tabs/Sources/Helpers/ZBSourceManager.h>
+
+@interface ZBHomeViewController () {
+    NSArray <ZBPackage *> *featuredPackages;
+    NSArray <NSDictionary *> *redditPosts;
+}
 
 @end
 
@@ -23,6 +31,8 @@
     
     if (self) {
         self.title = NSLocalizedString(@"Home", @"");
+        
+        [self downloadFeaturedPackages:NO];
     }
     
     return self;
@@ -42,5 +52,107 @@
     ZBMainSettingsTableViewController *settingsController = [storyboard instantiateViewControllerWithIdentifier:@"settingsNavController"];
     [[self navigationController] presentViewController:settingsController animated:YES completion:nil];
 }
+
+#pragma mark - Featured Packages
+
+- (void)downloadFeaturedPackages:(BOOL)useCaching {
+    NSArray *sources = [[ZBSourceManager sharedInstance] sources];
+    dispatch_group_t downloadGroup = dispatch_group_create();
+
+    for (ZBSource *source in sources) {
+        dispatch_group_enter(downloadGroup);
+
+        NSURLSession *session;
+        NSString *filePath = [[ZBAppDelegate listsLocation] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_Featured", source.baseFilename]];
+        if (useCaching && [[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+            NSError *fileError;
+            NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:&fileError];
+            NSDate *date = fileError != nil ? [NSDate distantPast] : [attributes fileModificationDate];
+
+            NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+            NSTimeZone *gmt = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+            [formatter setTimeZone:gmt];
+            [formatter setDateFormat:@"E, d MMM yyyy HH:mm:ss"];
+
+            NSString *modificationDate = [NSString stringWithFormat:@"%@ GMT", [formatter stringFromDate:date]];
+
+            NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+            configuration.HTTPAdditionalHeaders = @{@"If-Modified-Since": modificationDate};
+
+            session = [NSURLSession sessionWithConfiguration:configuration];
+        }
+        else {
+            session = [NSURLSession sharedSession];
+        }
+
+        NSMutableArray *featuredPackages = [NSMutableArray new];
+
+        NSURL *featuredURL = [NSURL URLWithString:@"sileo-featured.json" relativeToURL:source.mainDirectoryURL];
+        NSURLSessionDataTask *task = [session dataTaskWithURL:featuredURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+            if (error != NULL) {
+                ZBLog(@"[Zebra] Error while downloading featured JSON for %@: %@", source, error);
+                dispatch_group_leave(downloadGroup);
+                return;
+            }
+
+            if (data != NULL && [httpResponse statusCode] != 404) {
+                NSError *jsonReadError;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonReadError];
+                if (jsonReadError != NULL) {
+                    ZBLog(@"[Zebra] Error while parsing featured JSON for %@: %@", source, jsonReadError);
+                    dispatch_group_leave(downloadGroup);
+                    return;
+                }
+
+                if ([json objectForKey:@"banners"]) {
+                    NSArray *banners = [json objectForKey:@"banners"];
+                    if (banners.count) {
+                        [featuredPackages addObjectsFromArray:banners];
+                    }
+                }
+            }
+
+            NSString *filename = [NSString stringWithFormat:@"%@_Featured", source.baseFilename];
+            if ([featuredPackages count] > 0) {
+                [featuredPackages writeToFile:[[ZBAppDelegate listsLocation] stringByAppendingPathComponent:filename] atomically:true];
+            }
+
+            dispatch_group_leave(downloadGroup);
+        }];
+
+        [task resume];
+    }
+
+    dispatch_group_notify(downloadGroup, dispatch_get_main_queue(), ^{
+        [self chooseFeaturedPackages];
+    });
+}
+
+- (void)chooseFeaturedPackages {
+    NSArray *dirFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:[ZBAppDelegate listsLocation] error:nil];
+    NSArray *featuredCacheFiles = [dirFiles filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self ENDSWITH '_Featured'"]];
+
+    NSMutableArray *choices = [NSMutableArray new];
+    for (NSString *path in featuredCacheFiles) {
+        NSArray *contents = [NSArray arrayWithContentsOfFile:[[ZBAppDelegate listsLocation] stringByAppendingPathComponent:path]];
+        [choices addObjectsFromArray:contents];
+    }
+    
+    // Choose 5 random packages from the choices
+    NSUInteger numberOfPackages = MIN(choices.count, 5); // Configurable later maybe?
+    NSMutableSet *selection = [NSMutableSet new];
+
+    while (selection.count < numberOfPackages) {
+        id randomObject = [choices objectAtIndex:(arc4random() % choices.count)];
+        [selection addObject:randomObject];
+    }
+
+    featuredPackages = [selection allObjects];
+    NSLog(@"random choices %@", featuredPackages);
+}
+
+
+#pragma mark - Community News
 
 @end
